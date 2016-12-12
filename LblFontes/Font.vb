81df59f67ed788e97086451edbbf22ee8472afc3
bypedro <bypedro12@hotmail.com>
2016-12-12 23:10:15 +0000

    Inicio
    Adicionado Propriedades zTamanhoLetra e zTexto

---
+++ LblFontes/Font.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Drawing.Text
+Imports System.Runtime.InteropServices
+Imports System.Drawing
+''Código Baseado em http://zerosandtheone.com/blogs/vb/archive/2009/11/20/vb-net-include-a-font-as-an-embedded-resource-in-your-application.aspx
+Module CustomFont
+    'PRIVATE FONT COLLECTION TO HOLD THE DYNAMIC FONT
+    Private _pfc As PrivateFontCollection = Nothing
+
+    Public ReadOnly Property GetInstance(ByVal Size As Single, ByVal style As FontStyle) As Font
+        Get
+            'IF THIS IS THE FIRST TIME GETTING AN INSTANCE
+            'LOAD THE FONT FROM RESOURCES
+            If _pfc Is Nothing Then LoadFont()
+            'RETURN A NEW FONT OBJECT BASED ON THE SIZE AND STYLE PASSED IN
+            Return New Font(_pfc.Families(0), Size, style)
+        End Get
+    End Property
+    Private Sub LoadFont()
+        Try
+            'INIT THE FONT COLLECTION
+            _pfc = New PrivateFontCollection
+
+            'LOAD MEMORY POINTER FOR FONT RESOURCE
+            Dim fontMemPointer As IntPtr = Marshal.AllocCoTaskMem(My.Resources.maven.Length)
+
+            'COPY THE DATA TO THE MEMORY LOCATION
+            Marshal.Copy(My.Resources.maven, 0, fontMemPointer, My.Resources.maven.Length)
+
+            'LOAD THE MEMORY FONT INTO THE PRIVATE FONT COLLECTION
+            _pfc.AddMemoryFont(fontMemPointer, My.Resources.maven.Length)
+
+            'FREE UNSAFE MEMORY
+            Marshal.FreeCoTaskMem(fontMemPointer)
+        Catch ex As Exception
+            'ERROR LOADING FONT. HANDLE EXCEPTION HERE
+        End Try
+    End Sub
+End Module
---
+++ LblFontes/LblFontes.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Text
+
+Public Class LblFontes
+    Public Property zTamanhoLetra As Integer
+        Get
+            zTamanhoLetra = LblTexto.Font.Size
+        End Get
+        Set(ByVal value As Integer)
+            LblTexto.Font = CustomFont.GetInstance(value, FontStyle.Bold)
+        End Set
+    End Property
+    Public Property zTexto As String
+        Get
+            zTexto = LblTexto.Text
+        End Get
+        Set(ByVal value As String)
+            LblTexto.Text = value
+        End Set
+    End Property
+    Private Sub LblFontes_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LblTexto.Font = CustomFont.GetInstance(zTamanhoLetra, FontStyle.Bold)
+    End Sub
+End Class
